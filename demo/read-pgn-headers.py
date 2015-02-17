d053dc7fcc84a0541ab579bc824b454d383e5f54
Dave Spencer <david.spencerian@gmail.com>
2015-02-17 01:36:46 +0000

    decent speed on just header reading

---
+++ demo/read-pgn-headers.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import chess.pgn
+import sys
+
+for fn in sys.argv[1:]:
+    for offset, headers in chess.pgn.scan_headers(file(fn)):
+        print headers['WhiteElo'],headers['BlackElo'],headers['Result']
+    
