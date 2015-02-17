f40938d672cdc2f597bb0f66acddf4f4feda2d1a
Dave Spencer <david.spencerian@gmail.com>
2015-02-17 01:59:59 +0000

    event

--- demo/read-pgn-headers.py
+++ demo/read-pgn-headers.py
@@ -5,5 +5,5 @@ import sys
 
 for fn in sys.argv[1:]:
     for offset, headers in chess.pgn.scan_headers(file(fn)):
-        print headers['WhiteElo'],headers['BlackElo'],headers['Result']
+        print headers['Event'],headers['WhiteElo'],headers['BlackElo'],headers['Result']
     
