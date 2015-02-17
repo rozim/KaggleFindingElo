f57055c3a87b46ae0772c07bb1bbf28256acca4a
Dave Spencer <david.spencerian@gmail.com>
2015-02-17 02:18:36 +0000

    Add ply count, and generate CSV file for test data too.

--- generate-static.py
+++ generate-static.py
@@ -3,11 +3,28 @@
 import chess.pgn
 import sys
 
-# TBD: Ply
-print "#Event,WhiteElo,BlackElo,Result"
+print "#Event,WhiteElo,BlackElo,Ply,Result"
 for fn in sys.argv[1:]:
-    for offset, headers in chess.pgn.scan_headers(file(fn)):
-        print "%s,%d,%d,%s" % (headers['Event'],
-                               int(headers['WhiteElo']),
-                               int(headers['BlackElo']),
-                               headers['Result'])
+    f = file(fn)
+    while True:
+        game = chess.pgn.read_game(f)
+
+        if game is None:
+            print 'none'
+            break
+        headers = game.headers        
+        node = game.variation(0)
+        ply = 1
+        while node.variations:
+            node = node.variations[0]
+            ply += 1
+        try:
+            print "%s,%d,%d,%d,%s" % (headers['Event'],
+                                      int(headers['WhiteElo']),
+                                      int(headers['BlackElo']),
+                                      ply,
+                                      headers['Result'])
+        except KeyError:
+            print "%s,,,%d,%s" % (headers['Event'],
+                                  ply,
+                                  headers['Result'])            
