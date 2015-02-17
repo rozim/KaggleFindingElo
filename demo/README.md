b6dd950bdde4a49d7411ebade7d054e630aa5aa8
Dave Spencer <david.spencerian@gmail.com>
2015-02-17 01:29:00 +0000

    misc timing

--- demo/README.md
+++ demo/README.md
@@ -8,3 +8,9 @@ time ./read-pgn.py ../data/data-1000.pgn > /dev/null
 real	5m0.530s
 user	4m30.424s
 sys	0m0.164s
+
+Or without a node.board().fen() call
+
+real	3m4.773s
+user	2m57.872s
+sys	0m0.096s
--- demo/read-pgn.py
+++ demo/read-pgn.py
@@ -11,11 +11,12 @@ for fn in sys.argv[1:]:
             break
         node = game.variation(0)
         while not node.board().is_game_over():
-            print 'san: ', node.san(), ' move: ', node.move, 'fen: ', node.board().fen()
+            print 'san: ', node.san(), ' move: ', node.move
+            #node.board().fen()
             try:
                 node = node.variation(0)
             except KeyError:
                 break # there has to be a better way
         
-    
+#, 'fen: ', node.board().fen()    
     
