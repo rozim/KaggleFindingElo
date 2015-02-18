056c27d48d12e96caeba439a5a522165462dd2cd
Dave Spencer <david.spencerian@gmail.com>
2015-02-18 07:15:05 +0000

    decent uci chess engine interaction from python - code not perfect but so it goes

--- chess_util.py
+++ chess_util.py
@@ -1,6 +1,87 @@
+#!/usr/bin/python
+
+import sys
+import subprocess
 
 # Simplify the FEN string by stripping out the last 2 fields (half move clock and full move #)
 # under the assumption that these fields are not signifcant in distinguishing positions when
 # we want a set of unique positions.
 def SimplifyFen(fen):
     return ' '.join(fen.split()[0:4])
+
+
+def SendCommand(p, cmd):
+    p.stdin.write(cmd + '\n')
+
+def ReadUntil(p, prefix):
+    all = []
+    while True:
+        line = p.stdout.readline().strip()
+        if line.startswith(prefix):
+            return all
+        all.append(line)
+
+def SendCommandAndWaitFor(p, cmd, prefix):
+    SendCommand(p, cmd)
+    return ReadUntil(p,prefix)
+
+def After(ar, s):
+    return ar[ar.index(s) + 1]
+
+def ParseScore(ar):
+    i = ar.index('score')
+    if ar[i + 1] == 'cp':
+        return int(ar[i + 2])
+    assert ar[i + 1] == 'mate'
+    n = int(ar[i + 2])
+    if n > 0:
+        return 10000 - n
+    else:
+        return -10000 - n
+
+
+def SetFenPosition(p, fen):
+    SendCommand(p, 'position fen ' + fen)
+
+def SetOption(p, name, value):
+    return SendCommand(p, 'setoption name ' + name + ' value ' + value)
+    
+def ClearHash(p):
+    SetOption(p, 'Clear Hash', 'on')
+
+def StartEngine(exe):    
+    p = subprocess.Popen('/home/dspencer/Stockfish/src/stockfish',
+                            stderr=file('stderr.txt', 'w'),
+                            stdin=subprocess.PIPE,               
+                            stdout=subprocess.PIPE)
+    SendCommandAndWaitFor(p, 'uci', 'uciok')
+    return p
+
+def ParseLine(ar):
+    return {'depth' : After(ar, 'depth'),
+           'time': int(After(ar, 'time')),
+           'nodes': int(After(ar, 'nodes')),
+           'score': ParseScore(ar),
+           'pv': ar[ar.index('pv') + 1:]}
+
+def SearchDepth(p, depth):
+    ClearHash(p)
+    lines = SendCommandAndWaitFor(p, 'go depth %d' % depth, 'bestmove')
+    nodes = []
+    for line in lines:
+
+        ar = line.split()
+        if (ar[0] != 'info' or
+            ar[1] != 'depth' or
+            'upperbound' in ar or
+            'lowerbound' in ar or
+            not 'multipv' in ar or
+            not 'score' in ar or
+            not 'time' in ar or
+            not 'pv' in ar):
+            continue
+        
+        nodes.append(ParseLine(ar))
+    return nodes
+
+    
---
+++ stockfish.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from chess_util import *
+
+p = StartEngine('/home/dspencer/Stockfish/src/stockfish')
+
+SetOption(p, 'Hash', '128')
+
+# SetFenPosition(p, 'K7/5q2/k7/8/8/8/8/8 w - - 0 1')
+
+for line in SearchDepth(p, 20):
+    print line
+
+
