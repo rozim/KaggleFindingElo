e065edcdbd763b73320393cd3e520bd439d897a1
Dave Spencer <david.spencerian@gmail.com>
2015-02-19 16:31:58 +0000

    basic use of gflags for depth/hash size/multipv

--- generate-analysis.py
+++ generate-analysis.py
@@ -2,28 +2,45 @@
 
 from chess_util import *
 import time
+import gflags
+  
+FLAGS = gflags.FLAGS
+  
+gflags.DEFINE_integer('depth', 1, 'Depth to search')
+gflags.DEFINE_integer('hash', 128, 'Hash table size')
+gflags.DEFINE_integer('multipv', 1, 'Moves to search')
 
-depth = 9
 
-p = StartEngine('/home/dspencer/Stockfish/src/stockfish')
+def main(argv):
+    try:
+      argv = FLAGS(argv)  # parse flags
+    except gflags.FlagsError, e:
+      print '%s\\nUsage: %s ARGS\\n%s' % (e, sys.argv[0], FLAGS)
+      sys.exit(1)
 
-SetOption(p, 'Hash', '128')
-#SetOption(p, 'MultiPV', '5')
-# 
-positions = 0
-t1 = time.time()
-for fn in sys.argv[1:]:
-    with file(fn) as f:
-        for line in f.readlines():
-            (fen,moves) = line.split(',')
-            SetFenPosition(p, fen)
-            #print 'AN: ', fen
-            # {'pv': ['f7f5', 'e4f5'], 'depth': '2', 'score': -32, 'time': 2, 'nodes': 223, 'multipv': 1}
-            positions += 1
-            for depth_report in SearchDepth(p, depth):
-                #print '\t', depth_report
-                pass
-            if positions % 100 == 0:
-                print "%d. %.1f" % (positions, time.time() - t1)
-            if positions > 10000:
-                break
+    p = StartEngine('/home/dspencer/Stockfish/src/stockfish')
+
+    SetOption(p, 'Hash', '%d' % FLAGS.hash)
+    if FLAGS.multipv > 1:
+        SetOption(p, 'MultiPV', '%d' % FLAGS.multipv)
+
+    positions = 0
+    t1 = time.time()
+    for fn in argv[1:]:
+        print 'OPEN', fn
+        with file(fn) as f:
+            for line in f.readlines():
+                (fen,moves) = line.split(',')
+                SetFenPosition(p, fen)
+                # {'pv': ['f7f5', 'e4f5'], 'depth': '2', 'score': -32, 'time': 2, 'nodes': 223, 'multipv': 1}
+                positions += 1
+                for depth_report in SearchDepth(p, FLAGS.depth):
+                    print depth_report
+                print
+                if positions % 100 == 0:
+                    print "%d. %.1f" % (positions, time.time() - t1)
+                if positions > 10000:
+                    break    
+
+if __name__ == '__main__':
+    main(sys.argv)
