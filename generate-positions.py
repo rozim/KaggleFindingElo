d5dde959108cacef88aab902f1efecda9c4118a7
Dave Spencer <david.spencerian@gmail.com>
2015-02-19 21:37:58 -0800

    take dir name as arg too and look for *.json

--- generate-positions.py
+++ generate-positions.py
@@ -4,14 +4,23 @@ import cjson
 import sys
 import collections
 import chess_util
+import os.path
+import glob
 
 m = collections.defaultdict(set)
 
-for fn in sys.argv[1:]:
+def ProcessFile(fn, m):
     with file(fn) as f:
         obj = cjson.decode(f.read())
         for ply, pos in enumerate(obj['positions']):
-            m[chess_util.SimplifyFen(pos['fen'])].add(pos['move'])
+            m[chess_util.SimplifyFen(pos['fen'])].add(pos['move'])    
+
+for fn in sys.argv[1:]:
+    if os.path.isdir(fn):
+        for fn in glob.glob(fn + '/*.json'):
+            ProcessFile(fn, m)
+    else:
+        ProcessFile(fn, m)
 
 for fen, moves in m.iteritems():
     print '%s,%s' % (fen, ':'.join(moves))
