45dc692f9492b10bbca58cab6543f474dbdfd4b2
Dave Spencer <david.spencerian@gmail.com>
2015-02-24 09:23:48 -0800

    turn xgboost predictions into a submission

---
+++ form-submission.py
@@ -0,0 +1,18 @@
+
+
+print 'Event,WhiteElo,BlackElo'
+
+event = 25001
+with file('pred.txt') as f:
+    row = 0
+    white = black = 0
+    for line in f.readlines():
+        line = line.strip()
+        if row % 2 == 0:
+            white = float(line)
+        else:
+            black = float(line)
+            print '%d,%.0f,%.0f' % (event, black, white)
+            black = white = 0
+            event += 1
+        row += 1
