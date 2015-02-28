5e51955ee1d164378678fb4039d96ee3749706fb
Dave Spencer <david.spencerian@gmail.com>
2015-02-27 16:20:01 -0800

    use parallel

--- d2.sh
+++ d2.sh
@@ -1,5 +1,5 @@
 d=generated/analysis/2
 (cd generated/positions; ls | grep shard_ | sed -e s/shard_// | sed -e s/.txt// ) | while read fn
 do
-    ./generate-analysis.py --depth=2 --output=${d}/shard_${fn}-res.txt --done=${d}/shard_${fn}-done.txt generated/positions/shard_${fn}.txt
-done
+    echo ./generate-analysis.py --depth=2 --output=${d}/shard_${fn}-res.txt --done=${d}/shard_${fn}-done.txt generated/positions/shard_${fn}.txt
+done | parallel -j+0 --eta
