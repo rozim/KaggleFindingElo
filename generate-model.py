aa53fb7a638c41b109ffcf3d2f49f8ba5b3221a9
Dave Spencer <david.spencerian@gmail.com>
2015-03-01 14:05:37 -0800

    rework

--- generate-model.py
+++ generate-model.py
@@ -14,13 +14,15 @@ from collections import namedtuple
 
 FLAGS = gflags.FLAGS
 
-gflags.DEFINE_string('analysis', 'd13.leveldb', ("""Analysis database.
+gflags.DEFINE_string('analysis1', 'd1.leveldb', ("""Analysis database.
+                                                Key = 'simple FEN' """ ))
+gflags.DEFINE_string('analysis13', 'd13.leveldb', ("""Analysis database.
                                                 Key = 'simple FEN' """ ))
 
 gflags.DEFINE_string('model_dir', '.', '')
 gflags.DEFINE_integer('limit', 1000, '')
 
-(files, hit, miss, n_best_move, n_not_best_move) = (0, 0, 0, 0, 0)
+files = 0
 
 def safe_max(ar):
     if len(ar) == 0:
@@ -92,96 +94,97 @@ class Analysis(object):
 
 # Static info about a game
 GameInfo = namedtuple('GameInfo', ['event',
-                                   'best_count',
-                                   'best_pct',
                                    'game_ply',
                                    'white_elo',
                                    'black_elo',
                                    'result',
                                    'is_mate',
                                    'co_elo',
-                                   'co_deltas',
-                                   'opening',
+                                   'co_deltas_d1',
+                                   'co_deltas_d13',
+                                   'co_scores_d1',
+                                   'co_scores_d13',
                                    'co_result',
-                                   'first_loss_100',
-                                   'first_loss_200',
-                                   'first_loss_300'
+                                   'first_loss_d1',
+                                   'first_loss_d13'
                                    ])
 
-def StudyGame(db, opening_positions, fn):
-    global hit, miss, n_best_move, n_not_best_move
+# yields ply, co, position, analysis
+def GenerateAnalysis(db, game):
+    for ply, pos in enumerate(game.positions):
+        co = ply % 2
+        simple = chess_util.SimplifyFen(pos.fen)
+
+        try:
+            raw = db.Get(simple)
+        except KeyError:
+            continue
+        simple_pos = simple.split(' ')[0]
+        analysis = GameAnalysis(cjson.decode(raw))
+        yield ply, co, pos, analysis
+
+
+
+def FindBestLine(analysis):
+    move_map = {}
+    best_line = None
+    for i, line in enumerate(analysis.analysis):
+        if line.depth != analysis.depth:
+            continue
+        # First line found at target depth must be the best
+        if best_line is None:
+            best_line = line
+        move_map[line.pv[0]] = line.score
+    return (best_line, move_map)
+
+def CalculateDeltasAndScores(mega):
+    deltas = [[], []]
+    scores = [[], []]
+    for ply, co, pos, analysis in mega:
+        (best_line, move_map) = FindBestLine(analysis)
+        best_move =  best_line.pv[0]
+        scores[co].append([move_map[best_move], move_map[pos.move]])
+        if pos.move == best_move:
+            deltas[co].append(0)
+        else:
+            delta = abs(move_map[best_move] - move_map[pos.move])
+            if delta == 0:
+                # Regan gives a correction of -0.03 if an equal move was chosen
+                # but which wasn't the 1st rank.
+                deltas[co].append(3)
+            else:
+                deltas[co].append(max(3, delta))
+    return deltas, scores
+
+def CalculateFirstLoss(deltas):
+    (first_loss_100, first_loss_200, first_loss_300) = (0, 0, 0)
+    for ply, delta in enumerate(deltas):
+        if delta >= 100 and first_loss_100 == 0:
+            first_loss_100 = ply
+        if delta >= 200 and first_loss_200 == 0:
+            first_loss_200 = ply
+        if delta >= 300 and first_loss_300 == 0:
+            first_loss_300 = ply
+    return (first_loss_100, first_loss_200, first_loss_300)
+
+def StudyGame(db1, db13, opening_positions, fn):
     with file(fn) as f:
         game = Game(f)
 
-        best_count = [0, 0]
-        best_try = [0, 0]
-        deltas = [[], []]
-        first_loss_100 = [0, 0]
-        first_loss_200 = [0, 0]
-        first_loss_300 = [0, 0]
-        opening = set()
-        for ply, pos in enumerate(game.positions):
-            co = ply % 2
-            simple = chess_util.SimplifyFen(pos.fen)
+        mega_d1 = list(GenerateAnalysis(db1, game))
+        mega_d13 = list(GenerateAnalysis(db13, game))
+        (deltas_d1, scores_d1) = CalculateDeltasAndScores(mega_d1)
+        (deltas_d13, scores_d13) = CalculateDeltasAndScores(mega_d13)
+        first_loss_d1 = [CalculateFirstLoss(deltas_d1[0]), CalculateFirstLoss(deltas_d1[1])]
+        first_loss_d13 = [CalculateFirstLoss(deltas_d13[0]), CalculateFirstLoss(deltas_d13[1])]
 
-            try:
-                raw = db.Get(simple)
-                hit += 1
-            except KeyError:
-                miss += 1
-                continue
-            simple_pos = simple.split(' ')[0]
-            if simple_pos in opening_positions:
-                opening.add(simple_pos)
-            analysis = GameAnalysis(cjson.decode(raw))
-            target_depth = analysis.depth
-            move_map = {}
-            best_line = None
-            for i, line in enumerate(analysis.analysis):
-                if line.depth != target_depth:
-                    continue
-                # First line found at target depth must be the best
-                if best_line is None:
-                    best_line = line
-                move_map[line.pv[0]] = line.score
-
-            best_pv = best_line.pv
-            best_move = best_pv[0]
-            best_try[co] += 1
-            if pos.move == best_move:
-                n_best_move += 1
-                best_count[co] += 1
-            else:
-                n_not_best_move += 1
-                delta = abs(move_map[best_move] - move_map[pos.move])
-                if delta >= 100 and first_loss_100[co] == 0:
-                    first_loss_100[co] = ply
-                if delta >= 200 and first_loss_200[co] == 0:
-                    first_loss_200[co] = ply
-                if delta >= 300 and first_loss_300[co] == 0:
-                    first_loss_300[co] = ply                    
-
-                if delta == 0:
-                    # Regan gives a correction of -0.03 if an equal move was chosen
-                    # but which wasn't the 1st rank.
-                    deltas[co].append(3)
-                else:
-                    deltas[co].append(max(3, delta))
-
-        result = ParseResult[game.result]
-        best_pct = [0.0, 0.0]
-        if best_try[0] > 0:
-            best_pct[0] = float(best_count[0]) / best_try[0]
-        if best_try[1] > 0:
-            best_pct[1] = float(best_count[1]) / best_try[1]
         return GameInfo(game_ply = game.game_ply,
-                        first_loss_100 = first_loss_100,
-                        first_loss_200 = first_loss_200,
-                        first_loss_300 = first_loss_300,
-                        opening = opening,
-                        co_deltas = deltas,
-                        best_count = best_count,
-                        best_pct = best_pct,
+                        first_loss_d1 = first_loss_d1,
+                        first_loss_d13 = first_loss_d13,
+                        co_deltas_d1 = deltas_d1,
+                        co_deltas_d13 = deltas_d13,
+                        co_scores_d1 = scores_d1,
+                        co_scores_d13 = scores_d13,
                         event = game.event,
                         is_mate = game.is_mate,
                         white_elo = game.white_elo,
@@ -195,12 +198,12 @@ def StudyGame(db, opening_positions, fn):
 
 
 
-def ProcessArgs(db, opening_positions, limit, argv):
+def ProcessArgs(db1, db13, opening_positions, limit, argv):
     global files
 
     for event in range(1, limit + 1):
         fn = 'generated/game2json/%05d.json' % event
-        yield StudyGame(db, opening_positions, fn)
+        yield StudyGame(db1, db13, opening_positions, fn)
         files += 1
         if files >= limit:
             break
@@ -218,7 +221,7 @@ def ProcessDrawAndMate(gi):
     draw_ply = 0
     i_played_mate = [0, 0]
     i_was_mated = [0, 0]
-    
+
     if gi.result == 0.0:
         draw_ply = gi.game_ply
     elif gi.is_mate:
@@ -232,8 +235,8 @@ def ProcessDrawAndMate(gi):
             raise AssertionError()
     return (draw_ply, i_played_mate, i_was_mated)
 
-def ProcessDeltas(gi, co):
-    dampened_deltas = [min(300, delta) for delta in gi.co_deltas[co]]
+def ProcessDeltas(gi, co, deltas):
+    dampened_deltas = [min(300, delta) for delta in deltas]
     (delta_median, delta_stddev, delta_avg) = (0, 0, 0)
     if len(dampened_deltas) > 0:
         delta_avg = numpy.mean(dampened_deltas)
@@ -249,7 +252,8 @@ def main(argv):
       print '%s\\nUsage: %s ARGS\\n%s' % (e, sys.argv[0], FLAGS)
       sys.exit(1)
 
-    db = leveldb.LevelDB(FLAGS.analysis)
+    db1 = leveldb.LevelDB(FLAGS.analysis1)
+    db13 = leveldb.LevelDB(FLAGS.analysis13)
 
     if len(argv[1:]) == 0:
         print 'Need *.json or (generated/game2json/#####.json) dir (generated/game2json) arg'
@@ -257,47 +261,52 @@ def main(argv):
 
     opening_positions = ReadOpeningPositions('generated/position-frequency.csv')
 
-    for gi_num, gi in enumerate(ProcessArgs(db, opening_positions, FLAGS.limit, argv[1:])):
+    for gi_num, gi in enumerate(ProcessArgs(db1, db13, opening_positions, FLAGS.limit, argv[1:])):
         (draw_ply, i_played_mate, i_was_mated) = ProcessDrawAndMate(gi)
 
         for co in [0, 1]:
-            (delta_median, delta_stddev, delta_avg) = ProcessDeltas(gi, co)
+            (delta_median_d1, delta_stddev_d1, delta_avg_d1) = ProcessDeltas(gi, co, gi.co_deltas_d1[co])
+            (delta_median_d13, delta_stddev_d13, delta_avg_d13) = ProcessDeltas(gi, co, gi.co_deltas_d13[co])
 
             standard = {
-                '$g_event': gi.event,                
+                '$g_event': gi.event,
                 '$g_co_rating': gi.co_elo[co],
                 '$g_co': ["w", "b"][co],
-                '$g_co_deltas': gi.co_deltas[co],
-                
+                '$g_co_deltas_d1': gi.co_deltas_d1[co],
+                '$g_co_deltas_d13': gi.co_deltas_d13[co],
+                '$g_co_scores_d1': gi.co_scores_d1[co],
+                '$g_co_scores_d13': gi.co_scores_d13[co],
+
                 'color_value': [1, -1][co],
                 'game_ply': gi.game_ply,
                 'result': gi.co_result[co],
                 'draw_ply': draw_ply,
                 'i_played_mate': i_played_mate[co],
                 'i_was_mated': i_was_mated[co],
-                'best_count': gi.best_count[co],
-                'best_pct': gi.best_pct[co],
-                'delta_max': safe_max(gi.co_deltas[co]),
-                'delta_avg': delta_avg,
-                'delta_median': delta_median,
-                'delta_stddev': delta_stddev,
-                'first_loss_100': gi.first_loss_100[co],
-                'first_loss_200': gi.first_loss_200[co],
-                'first_loss_300': gi.first_loss_300[co]
+                'delta_max_d1': safe_max(gi.co_deltas_d1[co]),
+                'delta_max_d13': safe_max(gi.co_deltas_d13[co]),
+                'delta_avg_d1': delta_avg_d1,
+                'delta_median_d1': delta_median_d1,
+                'delta_avg_d13': delta_avg_d13,
+                'delta_median_d13': delta_median_d13,
+                'delta_stddev_d1': delta_stddev_d1,
+                'delta_stddev_d13': delta_stddev_d13,
+                'first_loss_100_d1': gi.first_loss_d1[co][0],
+                'first_loss_200_d1': gi.first_loss_d1[co][1],
+                'first_loss_300_d1': gi.first_loss_d1[co][2],
+                'first_loss_100_d13': gi.first_loss_d13[co][0],
+                'first_loss_200_d13': gi.first_loss_d13[co][1],
+                'first_loss_300_d13': gi.first_loss_d13[co][2]
             }
-            for pos in gi.opening:
-                standard['op_%s' % pos] = 1
             print cjson.encode(standard)
 
-
-    print
-    print "#Hit:            ", hit
-    print "#Miss:           ", miss
-    print "#Best move:      ", n_best_move
-    print "#Not best move:  ", n_not_best_move
-    print "#Files:          ", files
 
 
 if __name__ == '__main__':
     main(sys.argv)
 
+
+        #for ply, co, pos, analysis in mega_d1:
+        #simple_pos = chess_util.SimplifyFen(pos.fen).split(' ')[0]
+        #if simple_pos in opening_positions:
+        #opening.add(simple_pos)
