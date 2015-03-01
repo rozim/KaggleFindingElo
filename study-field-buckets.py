b2abe0e36b065652fd20ccd70327741a0eddaa1f
Dave Spencer <david.spencerian@gmail.com>
2015-02-28 20:20:54 -0800

    look for patterns by bucket

--- study-field-buckets.py
+++ study-field-buckets.py
@@ -5,6 +5,7 @@ import cjson
 import gflags
 import collections
 import numpy
+import random
 
 FLAGS = gflags.FLAGS
 gflags.DEFINE_string('in_model', 'model.xjson', 'Output of generate-model.py')
@@ -26,7 +27,7 @@ def ProcessModel(f):
         #print rating
         bucket = (rating / 100) * 100
         study[bucket].append(obj[FLAGS.field])
-        rev_study[int(obj[FLAGS.field])].append(bucket)
+        rev_study[int(obj[FLAGS.field])].append(rating)
     return study, rev_study
 
 def Report(study):
@@ -34,14 +35,19 @@ def Report(study):
     for bucket in sorted(study.keys()):
         ents = study[bucket]
         if len(ents) < 10:
-            continue        
-        print "%4d  | %6d | %4.0f | %4.0f | %4.0f | %4.0f | %4.0f" % (bucket,
+            continue
+        random.shuffle(ents)
+        sample = ' '.join(["%.0f" % _ for _ in sorted(ents[0:10])])
+        print "%4d  | %6d | %4.0f | %4.0f | %4.0f | %4.0f | %4.0f | %s" % (bucket,
                           len(ents),
                           numpy.min(ents),
                           numpy.max(ents),
                           numpy.median(ents),
                           numpy.mean(ents),
-                          numpy.std(ents))
+                          numpy.std(ents),
+                                                                      sample)
+                                                                      
+
 def main(argv):
     try:
       argv = FLAGS(argv)  # parse flags
